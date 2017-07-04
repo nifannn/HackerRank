ce08432458847e0836e8bdf0406a6d3f96c1ad5d
Fan Ni <ni.fan@outlook.com>
2017-07-04 15:51:53 -0400

    update
    update

---
+++ Practice/Java/Introduction/loops_1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1; i <= 10; i++){
+            System.out.printf("%d x %d = %d\n", N, i, N * i);
+        }
+    }
+}
